ab4e440b0f84267b7f1a676f9e9464816633c5b9
Tu883ifly <187037615+Tu883ifly@users.noreply.github.com>
2024-11-07 23:45:20 +0000

    Add unit test for HelloWorld functionality

---
+++ tests/HelloWorldTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Tests
+{
+    [TestClass]
+    public class HelloWorldTest
+    {
+        [TestMethod]
+        public void TestHelloWorld()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
